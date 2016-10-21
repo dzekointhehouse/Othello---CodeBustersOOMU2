6e483c2fe6cf32c8a3ad633f82c1725682b16954
optimusprime <optimusprime@10.10.140.204>
2016-10-21 14:46:26 +0200

    players ändrade + algoritm tillagd, funkar nästan

--- Grupp01Othello/src/grupp01othello/model/GameGrid.java
+++ Grupp01Othello/src/grupp01othello/model/GameGrid.java
@@ -8,30 +8,27 @@ package grupp01othello.model;
 import grupp01othello.view.GridObserver;
 import java.util.ArrayList;
 
-
-
 /**
  *
  * @author Markus, Elvir
  */
-public class GameGrid implements Subject{
+public class GameGrid implements Subject {
 
     private final int SIZE = 8;
     private int[][] grid;
     private ArrayList<GridObserver> observer;
+    ArrayList<Move> allLegalMoves;
 
-    /**
-     * Konstruktorn initialiserar game griden.
-     */
     public GameGrid() {
         grid = new int[SIZE][SIZE];
         observer = new ArrayList<GridObserver>();
+        allLegalMoves = new ArrayList<>();
 
     }
 
     /**
-     * initialiserar game griden till rätt storlek och
-     * med de fyra första pjäserna anpassat för othello
+     * initialiserar game griden till rätt storlek och med de fyra första
+     * pjäserna anpassat för othello
      */
     public void initiateGameGrid() {
         for (int row = 0; row < SIZE; row++) {
@@ -40,15 +37,14 @@ public class GameGrid implements Subject{
             }
 
         }
-
-        grid[3][4] = 2;
         grid[3][3] = 1;
-        grid[4][3] = 2;
+        grid[3][4] = 2;
         grid[4][4] = 1;
-        
-        notifyObserver(); // notifierar observer så GUI får pjäserna.
-    }
+        grid[4][3] = 2;
 
+        notifyObserver(); // notifierar observer så GUIn får start pjäserna.
+        GetAllLegalMoves(1);
+    }
 
     /**
      * boardIsFull loopar genom alla platser i matrisen och returnerar true om
@@ -95,66 +91,113 @@ public class GameGrid implements Subject{
         return white;
     }
 
-    public boolean isPossibleMove(int row, int col) {
-
-        return grid[row+1][col] != 0 || (grid[row-1][col])!=0 || (grid[row][col+1]) !=0 || (grid[row][col-1]) !=0;
-        
-    }
-
     @Override
     public void register(GridObserver newObserver) {
-        
+
         observer.add(newObserver);
     }
 
     @Override
     public void unregister(GridObserver deleteObserver) {
-        
+
         observer.remove(deleteObserver);
-        
+
     }
 
     @Override
     public void notifyObserver() {
-        
-        for(GridObserver observer : observer){
-            
+
+        for (GridObserver observer : observer) {
+
             observer.update(grid); // skickar uppdaterade spel griden till observers.
         }
     }
-  
-   public void updateGameGrid(Move move, int playerID){
-       
-       this.grid[move.getRow()][move.getColumn()] = playerID;
-       notifyObserver();
-   }
-   
-//   private void addToGrid(int player, int row, int col){
-//        for (int i = -1; i <= 1; i++) {
-//            for (int j = -1; j <= 1; j++) {
-//                recursiveAdd(player, row+i, col+j, i, j);
-//            }
-//        }
-//        grid[row][col] = player;
-//    } 
-// 
-//private boolean recursiveAdd(int colour, int row, int col, int rowChange, int colChange){
-//        if ((row >= || (col >= || (row < 0) || (col < 0))
-//            return false;
-//        if (grid[row][col] == 0)
-//            return false;
-//        if ((grid[row][col] == colour)){
-//            if (grid[row-rowChange][col-colChange] != 0)
-//                return true;
-//            else return false;
-//        }
-//        if (recursiveAdd(colour, row+rowChange, col+colChange, rowChange, colChange)){
-//            grid[row][col] = colour;
-//            return true;
-//        }
-//        else return false;
-//    }
 
+    public void playMove(Move move, int playerID) {
 
-    
+        processMove(playerID, move.getRow(), move.getColumn());
+        notifyObserver();
+    }
+
+    public void processMove(int player, int row, int col) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                flipChip(player, row + i, col + j, i, j);
+            }
+        }
+        grid[row][col] = player;
+        notifyObserver();
+    }
+
+    private boolean flipChip(int colour, int row, int col, int nextRow, int nextCol) {
+        if ((row < 0) || (col < 0) || (row >= 8) || (col >= 8)) {
+            return false;
+        }
+        if (grid[row][col] == 0) {
+            return false;
+        }
+        if ((grid[row][col] == colour)) {
+            if (grid[row - nextRow][col - nextCol] != 0 ) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (flipChip(colour, row + nextRow, col + nextCol, nextRow, nextCol)) {
+            grid[row][col] = colour;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean legalMove(int playerID, int row, int col) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (isLegalPath(playerID, row + i, col + j, i, j)) {
+                    System.out.println(row + " " + col);
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    private boolean isLegalPath(int colour, int row, int col, int nextRow, int nextCol) {
+        if ((row < 0) || (col < 0) || (row >= 8) || (col >= 8)) {
+            return false;
+        }
+        if (grid[row][col] == 0) {
+            return false;
+        }
+        if ((grid[row][col] == colour)) {
+            if (grid[row - nextRow][col - nextCol] != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (flipChip(colour, row + nextRow, col + nextCol, nextRow, nextCol)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ArrayList<Move> GetAllLegalMoves(int playerID) {
+
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (grid[row][col] == 0) { // så algoritmen inte testar sätta in från "existerande" pjäser.
+                    if (legalMove(playerID, row, col)) {
+                        allLegalMoves.add(new Move(row, col));
+                    }
+                }
+
+            }
+        }
+        return allLegalMoves;
+    }
+
 }
