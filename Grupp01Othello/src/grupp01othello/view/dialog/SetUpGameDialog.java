2d6c42e40668d56674c8566500d260c6f40145d7
optimusprime <optimusprime@10.10.140.204>
2017-02-12 13:59:35 +0100

    Setupgamedialogen ! :)

--- Grupp01Othello/src/grupp01othello/view/dialog/SetUpGameDialog.java
+++ Grupp01Othello/src/grupp01othello/view/dialog/SetUpGameDialog.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Initialiserar spelare.
  */
 package grupp01othello.view.dialog;
 
@@ -20,7 +18,7 @@ import javafx.scene.control.TextField;
 
 /**
  *
- * @author Markus
+ * @author Markus/Elvir 
  */
 public class SetUpGameDialog {
 
